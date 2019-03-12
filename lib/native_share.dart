2f7e528f0f9dffe35541979265c1f0faa091f26f
一维 <persenlee90@gmail.com>
2019-03-12 09:45:51 +0800

    format code

--- lib/native_share.dart
+++ lib/native_share.dart
@@ -2,13 +2,12 @@ import 'dart:async';
 
 import 'package:flutter/services.dart';
 
-
 class NativeShare {
   static const MethodChannel _channel =
       const MethodChannel('plugin.persenlee.com/native_share');
 
-  static Future<int> share(Map<String,dynamic> params) async{
-      var result = await _channel.invokeMethod('share',params);
-      return result;
+  static Future<int> share(Map<String, dynamic> params) async {
+    var result = await _channel.invokeMethod('share', params);
+    return result;
   }
 }
